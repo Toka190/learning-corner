bac48750723112c0ee43922ddae9472762b04e25
Toka190 <99836862+Toka190@users.noreply.github.com>
2023-09-05 01:14:23 +0300

    Add files via upload

---
+++ learn.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="de"> 
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="robots" content="index,follow">
+
+    <title>Museum|LEARN</title>
+
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/bootsnav.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+</head>
+
+<body>
+
+    <section id="inner-banner">
+        <div class="overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1>LEARN</h1>
+                    </div>
+                    <div class="col-sm-6">
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <section id="about-sec">
+        <div class="container">
+            <div class="row text-center">
+                <h1>LEARN CORNER</h1>
+                <hr>
+                <h5>The egyptians learning team create and deliver accessible world class cultural learning experiences for all ages and levels.</h5>
+                <p>Discover ancient civilisations, learn from the incredible achievements and challenges of people and societies past and present, and immerse yourself in the culture, languages, environments, people and debates of our ever-changing world.
+                    .
+                </p>
+                <p>organise studyrooms for schools and .</p>
+                <div class="text-center">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="help">
+        <div class="container">
+            <div class="row text-center">
+                <h1>studyroom details</h1>
+                <hr>
+                <h5>we are deligted to be welcoming back schools to museum.</h5>
+                <div class="text-left">
+                    <div class="col-md-6 clearfix top-off">
+                        <div class="help-text">
+                            <h4>Ages</h4>
+                            <div class="line line-50"></div>
+                            <p> 12+.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 clearfix top-off">
+                        <div class="help-text">
+                            <h4>Session time</h4>
+                            <div class="line line-50"></div>
+                            <p> 45 minutes.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 clearfix top-off">
+                        <div class="help-text">
+                            <h4>Maximum group size</h4>
+                            <div class="line line-50"></div>
+                            <p>120.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="team">
+        <div class="container">
+            <div class="row text-center">
+                <h1>School sessions</h1>
+                <hr>
+                <h5>provided sessions for this month:</h5>
+                <div class="member col-xs-6 col-md-3">
+                    <div class="inner">
+                        <div class="avatar-inner"><img src="images/se1.jpg"width="500px" height="300px"></div>
+                        <div class="info">
+                            <div class="name"><a href="#"> Roman in alexandria </a></div>
+                            <div class="regency">Learn about the Roman history in Egypt and the great monuments they left behind. <br>
+                            <ul style="color:black;">
+                                <li>status : avilable </li>
+                                <li>price :200L.E</li>
+                                 <li>start time : 4:00 pm, 11-11-2023</li>
+                                 <li>End time : 4:45 pm, 11-11-2023 </li>
+                                 <li>location: first floor, studyroom No.4</li>
+                            </ul>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="member col-xs-6 col-md-3">
+                    <div class="inner">
+                        <div class="avatar-inner"><img src="images/se2.jpg" width="500px" height="300px"></div>
+                        <div class="info">
+                            <div class="name"><a href="#"> Tomb of Tutankhamun </a></div>
+                            <div class="regency">The discovery that stunned millions around the world! <br>
+                            <ul style="color:black;">
+                                <li>status : avilable </li>
+                                <li>price :250L.E</li>
+                                 <li>start time : 4:00 pm, 12-11-2023</li>
+                                 <li>End time : 4:45 pm, 12-11-2023 </li>
+                                 <li>location: first floor, studyroom No.1</li>
+                            </ul>
+
+
+                           </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="member col-xs-6 col-md-3">
+                    <div class="inner">
+                        <div class="avatar-inner"><img src="images/se3.gif"width="500px" height="300px"></div>
+                        <div class="info">
+                            <div class="name"><a href="#"> Queen Cleopatra</a></div>
+                            <div class="regency">She was not black !  Come to learn more . <br><br>
+                            <ul style="color:black;">
+                                <li>status : avilable </li>
+                                <li>price :300L.E</li>
+                                 <li>start time : 4:00 pm, 15-11-2023</li>
+                                 <li>End time : 4:45 pm, 15-11-2023 </li>
+                                 <li>location: first floor, studyroom No.2</li>
+                            </ul>
+
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="member col-xs-6 col-md-3">
+                    <div class="inner">
+                        <div class="avatar-inner"><img src="images/se4.jpg"width="500px" height="300px"></div>
+                        <div class="info">
+                            <div class="name"><a href="#"> Resistance against Hyksos</a></div>
+                            <div class="regency">Egyptians would portray the Hyksos as bloodthirsty and oppressive foreign rulers. <br>
+                            <ul style="color:black;">
+                                <li>status : avilable </li>
+                                <li>price :400L.E</li>
+                                 <li>start time : 4:00 pm, 20-11-2023</li>
+                                 <li>End time : 4:45 pm, 20-11-2023 </li>
+                                 <li>location: first floor, studyroom No.4</li>
+                            </ul>
+                                                                </div>
+                                  
+                              </div>
+                            <br><br>                                  
+
+                    </div>
+                    
+                </div>
+                <h1>Enter session details   </h1>
+                                      <hr>
+                                      <form class="con-form clearfix" action="new.php" method="post"  >
+<div class="col-md-4">
+<input type="text" name="statue" value="" size="40" class="" id="name" aria-required="true" aria-invalid="false" placeholder="statue*">
+</div>
+<div class="col-md-4">
+<input type="text" name="price" value="" size="40" class="" aria-required="true" aria-invalid="false" placeholder="price">
+</div>
+<div class="col-md-4">
+<input type="text" name="start" value="" size="40" class="" id="subject" aria-invalid="false" placeholder="Start time ">
+</div><div class="col-md-4">
+<input type="text" name="end" value="" size="40" class="" id="subject" aria-invalid="false" placeholder="end time ">
+</div>
+<div class="col-md-4">
+<input type="text" name="location" value="" size="40" class="" id="subject" aria-invalid="false" placeholder="location ">
+</div>
+<div class="col-xs-12 submit-button">
+<input type="submit" name="submit" value="BOOK NOW" class="btn2" id="sub" style="border:none; margin: 20px 0 0 0">
+</div>
+
+</form>
+
+            </div>
+        </div>
+    </section>
+
+
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootsnav.js"></script>
+    <script src="js/banner.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ models/Learn.php
@@ -0,0 +1,13 @@
+<?php
+class Learn{
+    public $id;
+    public $statue;
+    public $price;
+    public $start;
+    public $end;
+    public $location;
+
+}
+
+
+?>
